d9ce4dfa830885fff4e8159cafcfb3feab41d01d
italoengdev <italo_saraiva182@hotmail.com>
2023-04-17 11:41:09 -0300

    adicionada extraçãp

--- main.py
+++ main.py
@@ -1,101 +1,67 @@
-from fastapi import FastAPI, Depends,  HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextBox
+import tabula
 import os
-from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import uvicorn
 
-load_dotenv()
-
-# Create the database URL
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-# Create a SQLAlchemy engine object
-engine = create_engine(DATABASE_URL)
-
-# Create a session factory
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-
-# Create a base class for database models
-Base = declarative_base()
-
-# Define a database model
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    email = Column(String)
-
-
-# Create the database tables
-Base.metadata.create_all(bind=engine)
-
-# Create the FastAPI app
 app = FastAPI()
-
-# Define a dependency for database sessions
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# Define an endpoint that adds a new user to the database
+templates = Jinja2Templates(directory="templates")
 
 
-@app.post("/users/")
-def create_user(username: str, email: str, db: Session = Depends(get_db)):
-    db_user = User(username=username, email=email)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return {"id": db_user.id, "username": db_user.username, "email": db_user.email}
-
-# Define an endpoint that lists all users
+@app.get("/")
+async def read_form(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
 
 
-@app.get("/users/")
-def list_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return [{"id": user.id, "username": user.username, "email": user.email} for user in users]
+@app.post("/extract/")
+async def extract_text(request: Request, file: UploadFile = File(...)):
+    # Save the uploaded PDF file to a "uploads" directory
+    with open(os.path.join("uploads", file.filename), "wb") as f:
+        f.write(file.file.read())
+
+    # Extract the text from the uploaded PDF file
+    pdf_path = os.path.join("uploads", file.filename)
+    with open(pdf_path, "rb") as f:
+        pdf_reader = PdfReader(f)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    # Render the "result.html" template with the extracted text
+    return templates.TemplateResponse("result.html", {"request": request, "filename": file.filename, "text": text})
 
 
-# Define an endpoint that retrieves a user by ID
+@app.post("/extract_diagrammed/")
+async def extract_diagrammed_text(request: Request, file: UploadFile = File(...)):
+    # Extract the text from the uploaded PDF file
+    pdf_path = os.path.join("uploads", file.filename)
+    text = ""
+    for page_layout in extract_pages(pdf_path):
+        for element in page_layout:
+            print(element)
+            if isinstance(element, LTTextBox):
+                text += element.get_text()
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"id": db_user.id, "username": db_user.username, "email": db_user.email}
-
-# Define an endpoint that updates a user by ID
-
-
-@app.put("/users/{user_id}")
-def update_user(user_id: int, username: str, email: str, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    db_user.username = username
-    db_user.email = email
-    db.commit()
-    db.refresh(db_user)
-    return {"id": db_user.id, "username": db_user.username, "email": db_user.email}
-
-# Define an endpoint that deletes a user by ID
+    return templates.TemplateResponse("result.html", {"request": request, "filename": file.filename, "text": text, "element": element})
 
 
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    db.delete(db_user)
-    db.commit()
-    return {"message": "User deleted successfully"}
+@app.post("/extract_tabular/")
+async def extract_tabular_text(request: Request, file: UploadFile = File(...)):
+
+    # Save the uploaded PDF file to a "uploads" directory
+    with open(os.path.join("uploads", file.filename), "wb") as f:
+        f.write(file.file.read())
+
+    # Extract the tables from the uploaded PDF file using Tabula
+    pdf_path = os.path.join("uploads", file.filename)
+    tables = tabula.read_pdf(pdf_path, pages="all")
+    for table in tables:
+        print(table.values.tolist())
+    return templates.TemplateResponse("result.html", {"request": request, "filename": file.filename, "table": table.values.tolist()})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
